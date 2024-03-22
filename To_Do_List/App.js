11fbee3a8955629791700a4aad9f6ec65792c36e
wajahatali <125338878+Wajahat981@users.noreply.github.com>
2024-03-22 12:19:46 -0700

    j
    j

--- To_Do_List/App.js
+++ To_Do_List/App.js
@@ -24,7 +24,7 @@ const App = () => {
     <SafeAreaView style={styles.container}>
       <TextInput
         style={styles.input}
-        placeholder="Add new todo list"
+        placeholder="Add new todo"
         value={text}
         onChangeText={setText}
       />
